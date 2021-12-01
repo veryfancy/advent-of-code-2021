794d84c74041fec28f48f1b33acc5a259157a537
Josh Schultz <veryfancy@users.noreply.github.com>
2021-12-01 20:25:25 +0000

    Day one part two

--- day-01/index.js
+++ day-01/index.js
@@ -17,13 +17,16 @@ function main() {
     console.log(measurements)
 
     let increases = 0;
-    for(let i=1; i<measurements.length; i++) {
-        if (measurements[i] > measurements[i-1]) {
+    for(let i=3; i<measurements.length; i++) {
+        let window1Sum = measurements[i-1] + measurements[i-2] + measurements[i-3];
+        let window2Sum = measurements[i] + measurements[i-1] + measurements[i-2];
+
+        if (window2Sum > window1Sum) {
             increases++;
         }
     }
 
-    console.log("increases:", increases)
+    console.log("increases:", increases);
 }
 
 main();
